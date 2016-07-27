da386eba0f2eb99dd8e1a81b70adb2e2e770a9e3
Robert Strandh <robert.strandh@gmail.com>
2016-07-27 16:51:23 +0200

    Initial version of package definition file.

---
+++ packages.lisp
@@ -0,0 +1,12 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:cluster
+  (:use #:common-lisp)
+  (:export
+   #:make-label
+   #:make-code-command
+   #:make-immediate-operand
+   #:make-gpr-operand
+   #:make-memory-operand
+   #:assemble))
+
