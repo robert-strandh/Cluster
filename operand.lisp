88bc69ccdb5f4176d9505ee89cc0e32dd413716e
Robert Strandh <robert.strandh@gmail.com>
2017-12-28 15:21:45 +0100

    Untabify for better tool support.

--- operand.lisp
+++ operand.lisp
@@ -53,105 +53,105 @@
 ;;; Always include the RXB bits of a potential REX byte.
 (defun encode-memory-operand (memory-operand)
   (with-accessors ((base-register base-register)
-		   (index-register index-register)
-		   (scale scale)
-		   (displacement displacement))
+                   (index-register index-register)
+                   (scale scale)
+                   (displacement displacement))
       memory-operand
     (cond ((and (null base-register)
-		(null index-register))
-	   ;; We have only a displacement.
-	   `(#b000
-	     #b00000101
-	     ,@(encode-integer displacement 4)))
-	  ((and (null index-register)
-		(null displacement))
-	   ;; We have only a base register.
-	   (multiple-value-bind (rex.b r/m)
-	       (floor base-register 8)
-	     (if (= r/m 4)
-		 `(,rex.b
-		   #b00000100  ; ModR/M byte.
-		   #b00100100) ; SIB byte.
-		 `(,rex.b
-		   ,r/m))))
-	  ((and (null index-register)
-		(typep displacement '(signed-byte 8)))
-	   ;; We have a base register and an 8-bit displacement.
-	   (multiple-value-bind (rex.b r/m)
-	       (floor base-register 8)
-	     (if (= r/m 4)
-		 `(,rex.b
-		   #b01000100 ; ModR/M byte.
-		   #b00100100 ; SIB byte.
-		   ,(encode-integer displacement 1))
-		 `(,rex.b
-		   ,(+ #b01000000 r/m)
-		   ,@(encode-integer displacement 1)))))
-	  ((and (null index-register)
-		(typep displacement '(signed-byte 32)))
-	   ;; We have a base register and a 32-bit displacement.
-	   (multiple-value-bind (rex.b r/m)
-	       (floor base-register 8)
-	     (if (= r/m 4)
-		 `(,rex.b
-		   #b10000100 ; ModR/M byte.
-		   #b00100100 ; SIB byte.
-		   ,(encode-integer displacement 4))
-		 `(,rex.b
-		   ,(+ #b10000000 r/m)
-		   ,@(encode-integer displacement 4)))))
-	  ((null base-register)
-	   ;; The only encoding provided when there is no base
-	   ;; register has a 32-bit displacement, so even if the
-	   ;; displacement is small or even 0, we must use this
-	   ;; encoding.
-	   (multiple-value-bind (rex.x i)
-	       (floor index-register 8)
-	     `(,(ash rex.x 1)
-	       #b00000100 ; ModR/M byte.
-	       ,(+ (ash (round (log scale 2)) 6)
-		   (ash i 3)
-		   #b101)
-	       ,@(encode-integer (or displacement 0) 4))))
-	  ((null displacement)
-	   (multiple-value-bind (rex.b b)
-	       (floor base-register 8)
-	     (multiple-value-bind (rex.x i)
-		 (floor index-register 8)
-	       (if (= b 5)
-		   ;; If the base register is 5 (EBP) or 13, then we
-		   ;; have a problem, because there is no encoding for
-		   ;; that situation without a displacement.  So we
-		   ;; use a displacement of 0.
-		   `(,(+ (ash rex.x 1) rex.b)
-		     #b01000100 ; ModR/M byte.
-		     ,(+ (ash (round (log scale 2)) 6)
-			 (ash i 3)
-			 b)
-		     0)
-		   `(,(+ (ash rex.x 1) rex.b)
-		     #b00000100 ; ModR/M byte.
-		     ,(+ (ash (round (log scale 2)) 6)
-			 (ash i 3)
-			 b))))))
-	  (t
-	   (multiple-value-bind (rex.b b)
-	       (floor base-register 8)
-	     (multiple-value-bind (rex.x i)
-		 (floor index-register 8)
-	       (if (typep displacement '(signed-byte 8))
-		   `(,(+ (ash rex.x 1) rex.b)
-		     #b01000100 ; ModR/M byte.
-		     ,(+ (ash (round (log scale 2)) 6)
-			 (ash i 3)
-			 b)
-		     ,@(encode-integer displacement 1))
-		   `(,(+ (ash rex.x 1) rex.b)
-		     #b10000100 ; ModR/M byte.
-		     ,(+ (ash (round (log scale 2)) 6)
-			 (ash i 3)
-			 b)
-		     ,@(encode-integer displacement 4)))))))))
+                (null index-register))
+           ;; We have only a displacement.
+           `(#b000
+             #b00000101
+             ,@(encode-integer displacement 4)))
+          ((and (null index-register)
+                (null displacement))
+           ;; We have only a base register.
+           (multiple-value-bind (rex.b r/m)
+               (floor base-register 8)
+             (if (= r/m 4)
+                 `(,rex.b
+                   #b00000100  ; ModR/M byte.
+                   #b00100100) ; SIB byte.
+                 `(,rex.b
+                   ,r/m))))
+          ((and (null index-register)
+                (typep displacement '(signed-byte 8)))
+           ;; We have a base register and an 8-bit displacement.
+           (multiple-value-bind (rex.b r/m)
+               (floor base-register 8)
+             (if (= r/m 4)
+                 `(,rex.b
+                   #b01000100 ; ModR/M byte.
+                   #b00100100 ; SIB byte.
+                   ,(encode-integer displacement 1))
+                 `(,rex.b
+                   ,(+ #b01000000 r/m)
+                   ,@(encode-integer displacement 1)))))
+          ((and (null index-register)
+                (typep displacement '(signed-byte 32)))
+           ;; We have a base register and a 32-bit displacement.
+           (multiple-value-bind (rex.b r/m)
+               (floor base-register 8)
+             (if (= r/m 4)
+                 `(,rex.b
+                   #b10000100 ; ModR/M byte.
+                   #b00100100 ; SIB byte.
+                   ,(encode-integer displacement 4))
+                 `(,rex.b
+                   ,(+ #b10000000 r/m)
+                   ,@(encode-integer displacement 4)))))
+          ((null base-register)
+           ;; The only encoding provided when there is no base
+           ;; register has a 32-bit displacement, so even if the
+           ;; displacement is small or even 0, we must use this
+           ;; encoding.
+           (multiple-value-bind (rex.x i)
+               (floor index-register 8)
+             `(,(ash rex.x 1)
+               #b00000100 ; ModR/M byte.
+               ,(+ (ash (round (log scale 2)) 6)
+                   (ash i 3)
+                   #b101)
+               ,@(encode-integer (or displacement 0) 4))))
+          ((null displacement)
+           (multiple-value-bind (rex.b b)
+               (floor base-register 8)
+             (multiple-value-bind (rex.x i)
+                 (floor index-register 8)
+               (if (= b 5)
+                   ;; If the base register is 5 (EBP) or 13, then we
+                   ;; have a problem, because there is no encoding for
+                   ;; that situation without a displacement.  So we
+                   ;; use a displacement of 0.
+                   `(,(+ (ash rex.x 1) rex.b)
+                     #b01000100 ; ModR/M byte.
+                     ,(+ (ash (round (log scale 2)) 6)
+                         (ash i 3)
+                         b)
+                     0)
+                   `(,(+ (ash rex.x 1) rex.b)
+                     #b00000100 ; ModR/M byte.
+                     ,(+ (ash (round (log scale 2)) 6)
+                         (ash i 3)
+                         b))))))
+          (t
+           (multiple-value-bind (rex.b b)
+               (floor base-register 8)
+             (multiple-value-bind (rex.x i)
+                 (floor index-register 8)
+               (if (typep displacement '(signed-byte 8))
+                   `(,(+ (ash rex.x 1) rex.b)
+                     #b01000100 ; ModR/M byte.
+                     ,(+ (ash (round (log scale 2)) 6)
+                         (ash i 3)
+                         b)
+                     ,@(encode-integer displacement 1))
+                   `(,(+ (ash rex.x 1) rex.b)
+                     #b10000100 ; ModR/M byte.
+                     ,(+ (ash (round (log scale 2)) 6)
+                         (ash i 3)
+                         b)
+                     ,@(encode-integer displacement 4)))))))))
 
 (defclass immediate-operand (operand)
   (;; A signed integer.
@@ -165,21 +165,21 @@
   (ecase (car descriptor)
     (gpr-a
      (and (typep operand 'gpr-operand)
-	  (= (code-number operand) 0)
-	  (= (size operand) (cadr descriptor))))
+          (= (code-number operand) 0)
+          (= (size operand) (cadr descriptor))))
     (gpr
      (and (typep operand 'gpr-operand)
-	  (= (size operand) (cadr descriptor))))
+          (= (size operand) (cadr descriptor))))
     (memory
      (and (typep operand 'memory-operand)
-	  (= (size operand) (cadr descriptor))))
+          (= (size operand) (cadr descriptor))))
     (simm
      (and (typep operand 'immediate-operand)
-	  (typep (value operand) `(signed-byte ,(cadr descriptor)))))
+          (typep (value operand) `(signed-byte ,(cadr descriptor)))))
     (imm
      (and (typep operand 'immediate-operand)
-	  (or (typep (value operand) `(signed-byte ,(cadr descriptor)))
-	      (typep (value operand) `(unsigned-byte ,(cadr descriptor))))))
+          (or (typep (value operand) `(signed-byte ,(cadr descriptor)))
+              (typep (value operand) `(unsigned-byte ,(cadr descriptor))))))
     (label
      ;; We don't take into account the size of the label at this
      ;; point, because we do not yet know what the final size of the
