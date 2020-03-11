09570e96c1e6ca66e87c60067769d5d8edf822d2
Robert Strandh <robert.strandh@gmail.com>
2020-03-11 15:28:46 +0100

    Add instruction definitions for 32-bit NEG.

--- x86-instruction-database/neg.lisp
+++ x86-instruction-database/neg.lisp
@@ -6,7 +6,29 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Nigate a 64-bit GPR or memory location.
+;;; Negate a 32-bit GPR or memory location.
+
+;;; Negate a 32-bit GPR.
+(define-instruction "NEG"
+  :modes (32 64)
+  :operands ((gpr 32))
+  :opcodes (#xF7)
+  :opcode-extension 3
+  :encoding (modrm)
+  :rex.w t)
+
+;;; Negate a 32-bit memory location
+(define-instruction "NEG"
+  :modes (32 )
+  :operands ((memory 32))
+  :opcodes (#xF7)
+  :opcode-extension 3
+  :encoding (modrm)
+  :lock t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Negate a 64-bit GPR or memory location.
 
 ;;; Negate a 64-bit GPR.
 (define-instruction "NEG"
