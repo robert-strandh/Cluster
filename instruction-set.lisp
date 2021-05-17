02b7cc1c32b27e76d808be3a82e33a23739fc73c
Gnuxie <gnuxie@protonmail.com>
2021-05-17 11:49:17 +0100

    generate interpreter state registers from instruction set

--- instruction-set.lisp
+++ instruction-set.lisp
@@ -91,6 +91,7 @@ associated with the instruction set."))
   (:documentation "Returns a list of encoding
 types that are associated with this instruction-set."))
 (defgeneric range-prefixes (instruction-set))
+(defgeneric set-name (instruction-set))
 
 (defclass standard-instruction-set (instruction-set)
   ((%modifier-prefixes :initarg :modifier-prefixes
@@ -98,7 +99,9 @@ types that are associated with this instruction-set."))
    (%encodings :initarg :encodings
                :accessor encodings)
    (%range-prefixes :initarg :range-prefixes
-                    :reader range-prefixes)))
+                    :reader range-prefixes)
+   (%set-name :initarg :set-name
+              :reader set-name)))
 
 (defparameter *instruction-sets* (make-hash-table))
 (defun find-instruction-set (name)
@@ -113,6 +116,7 @@ types that are associated with this instruction-set."))
                         (loop for prefix in ',modifier-prefixes
                               collect (find-modifier-prefix prefix))
                         :encodings ',encodings
+                        :set-name ',name
                         :range-prefixes
                         (loop for prefix in ',range-prefixes
                               collect (find-range-prefix prefix)))))
@@ -165,3 +169,93 @@ types that are associated with this instruction-set."))
     (add-prefixes
      (array-table<-list-of-tables (nreverse tables))
      instruction-set)))
+
+;;;;
+;;;; decoder interprete state mixin
+
+(defgeneric instruction-set (decoder-state))
+(defgeneric reset-state (decoder-state))
+(defclass decoder-state () ())
+
+(defun prefix-slot-name<-prefix (prefix)
+  (intern (format nil "%~w" (prefix-name prefix))))
+
+(defun prefix-accessor-name<-prefix (prefix)
+  "Possibly redundant because of the prefix predicate."
+  (intern (format nil "~w-PRESENT-P" (prefix-name prefix))))
+
+(defun decoder-register-slots<-modifier-prefixes (modifier-prefixes)
+  (loop for prefix in modifier-prefixes
+        collect `(,(prefix-slot-name<-prefix prefix)
+                  :accessor ,(prefix-accessor-name<-prefix prefix)
+                  :accessor ,(instruction-descriptor-predicate prefix)
+                  :initform nil)))
+
+(defun prefix-value-slot-name<-prefix (prefix)
+  (intern (format nil "%~w-VALUE" (prefix-name prefix))))
+
+(defun prefix-value-slot-accessor<-prefix (prefix)
+  (intern (format nil "~w-VALUE" (prefix-name prefix))))
+
+(defun decoder-register-slots<-range-prefixes (range-prefixes)
+  (loop for prefix in range-prefixes
+        collect `(,(prefix-value-slot-name<-prefix prefix)
+                  :accessor ,(prefix-value-slot-name<-prefix prefix)
+                  :initform nil)))
+
+(defun bitflag-accessors<-range-prefixes (set range-prefixes)
+  (flet ((method-definition (set range-prefix prefix)
+           `(defmethod ,(instruction-descriptor-predicate prefix)
+                ((state ,(interpreter-state-class-name<-instruction-set set)))
+              (logtest ',(prefix-opcode-bitmask prefix)
+                       (,(prefix-value-slot-accessor<-prefix range-prefix)
+                        state)))))
+    (loop for range-prefix in range-prefixes
+          append (loop for prefix in (bitflag-prefixes range-prefix)
+                       collect (method-definition set range-prefix prefix)))))
+
+(defun decoder-state-slots (instruction-set)
+  (list
+   `(%instruction-set
+     :reader instruction-set
+     :initform (find-instruction-set ',(set-name instruction-set))
+     :allocation :class)))
+
+(defun interpreter-state-class-name<-instruction-set (instruction-set)
+  (intern (format nil "~w-STATE" (set-name instruction-set))))
+
+(defun interpreter-state-class<-instruction-set (instruction-set direct-superclasses)
+  `(defclass ,(interpreter-state-class-name<-instruction-set instruction-set)
+       ,`(decoder-state . ,direct-superclasses)
+     ,(append
+       (decoder-register-slots<-modifier-prefixes
+        (modifier-prefixes instruction-set))
+       (decoder-register-slots<-range-prefixes
+        (range-prefixes instruction-set))
+       (decoder-state-slots instruction-set))))
+
+;;; TODO
+;;; do we need a presentp for a range prefix?
+(defun reset-state-method-definition<-instruction-set (instruction-set)
+  `(defmethod reset-state ((state ,(interpreter-state-class-name<-instruction-set
+                                    instruction-set)))
+     ,@ (loop for prefix in (modifier-prefixes instruction-set)
+              collect `(setf (,(prefix-accessor-name<-prefix prefix) state)
+                             nil))
+     ,@ (loop for prefix in (range-prefixes instruction-set)
+              collect `(setf (,(prefix-value-slot-accessor<-prefix prefix) state)
+                             0))))
+
+(defgeneric make-decoder/interpreter-state-definition
+    (instruction-set direct-superclasses)
+  (:method ((instruction-set instruction-set) direct-superclasses)
+    `(progn
+       ,(interpreter-state-class<-instruction-set instruction-set direct-superclasses)
+       ,(reset-state-method-definition<-instruction-set instruction-set)
+       ,@(bitflag-accessors<-range-prefixes instruction-set
+                                           (range-prefixes instruction-set)))))
+
+(defmacro define-deocder/interpreter (instruction-set-name (&rest direct-superclasses))
+  (make-decoder/interpreter-state-definition
+   (find-instruction-set instruction-set-name)
+   direct-superclasses))
