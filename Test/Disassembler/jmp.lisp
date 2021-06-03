ff03b77f017aaf7c6ace8cc60ee7af76b48528a4
Gnuxie <gnuxie@protonmail.com>
2021-06-03 12:40:46 +0100

    Use Cluster to asssemble instructions in JMP disassemble test

--- Test/Disassembler/jmp.lisp
+++ Test/Disassembler/jmp.lisp
@@ -1,16 +1,26 @@
 (cl:in-package #:cluster-test.disassembler)
 
-;;; TODO Cluster doesn't encode this instruction
-;;; using make-memory-operand properly
-(defun test-jmp-r/m32 ()
-  (let* ((jmp-ebx+edi*4 #(#xFF #b00100100 #b10111011))
-         (disassembled-code-command (decode-instruction jmp-ebx+edi*4)))
+(defun make-jmp-r/m (r/m-operand)
+  (let ((encoding
+          (c:compute-encoding (c:make-code-command
+                               "JMP"
+                               (list r/m-operand)))))
+    (make-array (length encoding) :initial-contents encoding)))
+
+(defun test-jmp-r/m64 ()
+  (let* ((jmp-rbx+rdi*4
+           (make-jmp-r/m
+            (c:make-memory-operand 64
+                                   :base-register 3
+                                   :index-register 7
+                                   :scale 4)))
+         (disassembled-code-command (decode-instruction jmp-rbx+rdi*4)))
     (assert-decoded-descriptor disassembled-code-command
                                :mnemonic "JMP"
                                :opcode-extension 4
-                               :operands '((c:memory 32)))
+                               :operands '((c:memory 64)))
     (assert-memory-operand (first (c:operands disassembled-code-command))
                            :base-register 3
                            :index-register 7
                            :scale 4
-                           :size 32)))
+                           :size 64)))
