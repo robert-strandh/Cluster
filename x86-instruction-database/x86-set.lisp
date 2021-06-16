1ae88fe4953245ec220e59a13db0a9b4309bf1b2
Gnuxie <gnuxie@protonmail.com>
2021-06-16 12:12:33 +0100

    Remove LOCK as a modifier prefix, is annotative only
    Before the disassembler was using the prescence of the LOCK prefix in an instruction to
    eliminate instruction descriptor candidates. This is the wrong behaviour at current because
    the LOCK slot on the instruction-descriptor is there to show that the descriptor supports
    LOCK, not that LOCK must be present in an encoded instruction

--- x86-instruction-database/x86-set.lisp
+++ x86-instruction-database/x86-set.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:cluster)
 
+;;; LOCK is annotative, it doesn't change the instruction descriptor
+;;; that is used, we also don't have a way to show LOCk
+;;; in code-commands in Cluster either atm
+#+ (or)
 (define-modifier-prefix lock lock #xF0)
 (define-modifier-prefix operand-size-override
   operand-size-override #x66)
@@ -8,5 +12,5 @@
   (#b1000 rex.w))
 
 (define-instruction-set x86
-    (:modifier-prefixes (lock operand-size-override)
+    (:modifier-prefixes (operand-size-override)
      :range-prefixes (rex)))
