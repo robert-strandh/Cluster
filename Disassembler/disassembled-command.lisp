e010f0e067da3417f74a1ff69be6aa2e96159866
Gnuxie <gnuxie@protonmail.com>
2021-08-23 19:05:59 +0100

    Use Cluster's code-command in Disassembler's disassembled-code-command.

--- Disassembler/disassembled-command.lisp
+++ Disassembler/disassembled-command.lisp
@@ -1,18 +1,20 @@
 (cl:in-package #:cluster.disassembler)
 
-(defclass disassembled-command (cluster:command)
+(defclass disassembled-command (cluster:code-command)
   ((%instruction-descriptor :initarg :instruction-descriptor
                             :reader instruction-descriptor)
-   (%operands :initarg :operands :reader cluster:operands)
    ;; The position of the instruction in the sequence it was decoded
    ;; from if indeed it was present in one.
    (%start-position :initarg :start-position :accessor start-position
                     :initform nil)))
 
-(defmethod cluster:mnemonic ((command disassembled-command))
-  (cluster:mnemonic (instruction-descriptor command)))
+(defmethod print-object ((object disassembled-command) stream)
+  (print-unreadable-object (object stream :type t :identity t)
+    (format stream "~a ~a" (c:mnemonic object)
+            (c:operands (instruction-descriptor object)))))
 
 (defun make-disassembled-command (instruction-descriptor operands)
   (make-instance 'disassembled-command
                  :instruction-descriptor instruction-descriptor
-                 :operands operands))
+                 :operands operands
+                 :mnemonic (c:mnemonic instruction-descriptor)))
